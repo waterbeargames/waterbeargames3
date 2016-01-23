93b61a2209ff47a7bec9f1837ef73f0ac63c70ab
Cara Heacock <caraheacock@gmail.com>
2016-01-23 02:22:09 -0600

    Fixes game headers for IE/Edge, fixes button target in news section

--- header-game.php
+++ header-game.php
@@ -9,7 +9,7 @@ if (!empty($game['banner'])) {
 <header id="header" class="alternative-background"<?php echo ($game_banner ? ' style="background-image: url(' . $game_banner . ');"' : ''); ?>>
     <?php if (!empty($game['logo'])) : ?>
     <div class="header-game-logo"<?php echo(!empty($game['accent_color']) ? ' style="background-color: rgba(' . hex2rgb($game['accent_color']) . ', 0.75);"' : ''); ?>>
-        <?php include(get_attached_file($game['logo'])); ?>
+        <?php insert_svg(get_attached_file($game['logo'])); ?>
     </div>
     <?php else : ?>
     <h1><?php the_title(); ?></h1>
