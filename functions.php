6b1dbc6bcea667cd59885be7b7a16dea6e27d86a
Cara Heacock <caraheacock@gmail.com>
2016-02-01 20:40:15 -0600

    Added version numbers to assets to bust caching

--- functions.php
+++ functions.php
@@ -39,14 +39,17 @@ add_action('widgets_init', 'sidebar_widget_init');
 // Add Styles and Scripts
 function puzzle_scripts() {
     wp_enqueue_style('google-fonts-open-sans', 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
-    wp_enqueue_style('puzzle-style', get_template_directory_uri() . '/assets/css/main.css');
+    $puzzle_style_location = '/assets/css/main.css';
+    wp_enqueue_style('puzzle-style', get_template_directory_uri() . $puzzle_style_location, array(), filemtime(get_stylesheet_directory() . $puzzle_style_location));
     
-    if (!file_exists(get_stylesheet_directory() . '/assets/css/custom.css')) {
+    $puzzle_custom_style_location = '/assets/css/custom.css';
+    if (!file_exists(get_stylesheet_directory() . $puzzle_custom_style_location)) {
         puzzle_save_custom_style();
     }
-    wp_enqueue_style('puzzle-custom-style', get_template_directory_uri() . '/assets/css/custom.css?' . filemtime(get_stylesheet_directory() . '/assets/css/custom.css'));
+    wp_enqueue_style('puzzle-custom-style', get_template_directory_uri() . $puzzle_custom_style_location, array(), filemtime(get_stylesheet_directory() . $puzzle_custom_style_location));
     
-    wp_enqueue_script('puzzle-script', get_template_directory_uri() . '/assets/js/main.js', array('jquery'));
+    $puzzle_script_location = '/assets/js/main.js';
+    wp_enqueue_script('puzzle-script', get_template_directory_uri() . $puzzle_script_location, array('jquery'), filemtime(get_stylesheet_directory() . $puzzle_script_location));
 }
 add_action('wp_enqueue_scripts', 'puzzle_scripts');
 
