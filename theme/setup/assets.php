d69fdd5f892435975faf920cbdde22aedde03456
Cara Heacock <caraheacock@gmail.com>
2016-09-12 18:58:57 -0500

    Revises asset names, removes protocol from Google Fonts so they'll work over https

--- theme/setup/assets.php
+++ theme/setup/assets.php
@@ -6,26 +6,31 @@
  */
 
 /* Add styles and scripts */
-function puzzle_scripts() {
-    wp_enqueue_style('google-fonts-open-sans', 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
-    $puzzle_style_location = '/assets/css/main.css';
-    wp_enqueue_style('puzzle-style', get_template_directory_uri() . $puzzle_style_location, array(), filemtime(get_stylesheet_directory() . $puzzle_style_location));
+function wbg_scripts() {
+    /* Google fonts */
+    wp_enqueue_style('wbg-google-fonts', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
     
-    $puzzle_custom_style_location = '/assets/css/custom.css';
-    if (!file_exists(get_stylesheet_directory() . $puzzle_custom_style_location)) {
+    /* Main styles */
+    $wbg_style_location = '/assets/css/main.css';
+    wp_enqueue_style('wbg-style', get_template_directory_uri() . $wbg_style_location, array(), filemtime(get_stylesheet_directory() . $wbg_style_location));
+    
+    /* Custom styles generated from user options */
+    $wbg_custom_style_location = '/assets/css/custom.css';
+    if (!file_exists(get_stylesheet_directory() . $wbg_custom_style_location)) {
         puzzle_save_custom_style();
     }
-    wp_enqueue_style('puzzle-custom-style', get_template_directory_uri() . $puzzle_custom_style_location, array(), filemtime(get_stylesheet_directory() . $puzzle_custom_style_location));
+    wp_enqueue_style('wbg-custom-style', get_template_directory_uri() . $wbg_custom_style_location, array(), filemtime(get_stylesheet_directory() . $wbg_custom_style_location));
     
-    $puzzle_script_location = '/assets/js/main.js';
-    wp_enqueue_script('puzzle-script', get_template_directory_uri() . $puzzle_script_location, array('jquery'), filemtime(get_stylesheet_directory() . $puzzle_script_location));
+    /* Main script */
+    $wbg_script_location = '/assets/js/main.js';
+    wp_enqueue_script('wbg-script', get_template_directory_uri() . $wbg_script_location, array('jquery'), filemtime(get_stylesheet_directory() . $wbg_script_location));
 }
-add_action('wp_enqueue_scripts', 'puzzle_scripts');
+add_action('wp_enqueue_scripts', 'wbg_scripts');
 
 /* Add admin styles */
-function puzzle_admin_styles() {
-    wp_enqueue_style('puzzle-admin-styles', get_template_directory_uri() . '/assets/css/admin-styles.css');
+function wbg_admin_styles() {
+    wp_enqueue_style('wbg-admin-styles', get_template_directory_uri() . '/assets/css/admin-styles.css');
 }
-add_action('admin_print_styles', 'puzzle_admin_styles');
+add_action('admin_print_styles', 'wbg_admin_styles');
 
 ?>
