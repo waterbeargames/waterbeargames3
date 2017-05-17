6ee303be75e0b91d01ff2158c0b84ff8333a1ff4
Cara Heacock <caraheacock@gmail.com>
2017-05-17 02:02:47 -0500

    Refactors footer

--- footer.php
+++ footer.php
@@ -1,20 +1,20 @@
     </main>
-    <?php
-    $footer = get_theme_mod('footer_content');
-
-    if ($footer) : ?>
-        <footer class="main-footer">
-            <div class="row">
-                <div class="col xs-12">
-                    <div class="col-inner">
-                        <?php echo apply_filters('like_the_content', wp_kses_post($footer)); ?>
-                    </div>
+    <?php $footer_content = get_theme_mod('footer_content'); ?>
+    <footer class="mainFooter">
+        <div class="row">
+            <div class="col xs-12">
+                <div class="col-inner">
+                    <p>&copy; <?php echo strftime('%Y') . ' ' . get_bloginfo(); ?></p>
+                    <?php
+                    if (isset($footer_content)) {
+                        echo apply_filters('like_the_content', wp_kses_post($footer_content));
+                    }
+                    ?>
                 </div>
             </div>
-        </footer>
-        <?php
-    endif;
-
+        </div>
+    </footer>
+    <?php
     // Drop shadow for SVGs such as game logos
     include('assets/images/dropshadow.svg');
 
